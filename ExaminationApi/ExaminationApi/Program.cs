e46a7897160cd26013370eb1ea90c6afef7cc5af
Hiba Ishal <Hibaishal@hotmail.com>
2026-02-06 22:01:17 +0100

    Tar bort https redirection för att lösa health check

--- ExaminationApi/ExaminationApi/Program.cs
+++ ExaminationApi/ExaminationApi/Program.cs
@@ -2,25 +2,21 @@ using ExaminationApi;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Lägger till tjänster som behövs för API:et
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Konfigurerar Swagger så vi kan testa funktionerna
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+// Tog bort app.UseHttpsRedirection() här för att AWS Health Check ska fungera.
 
-// En enkel GET endpoint för att testa att webbplatsen fungerar
 app.MapGet("/", () => "Webbplatsen fungerar!");
 
-// Här ligger logiken för översättnigen som anropas via API
 app.MapPost("/translate", (string text) =>
 {
     var translator = new Translator();
@@ -28,5 +24,4 @@ app.MapPost("/translate", (string text) =>
     return result;
 });
 
-// Startar applikationen och lyssnar på port 5000
 app.Run("http://0.0.0.0:5000");
